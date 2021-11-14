f9ab711cafe12e746a296414cab0ed730deeb17f
Salwa-mechergui <57394292+Salwa-mechergui@users.noreply.github.com>
2021-11-14 20:40:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrolly > 20){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-    })
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    //toggle navbar/menu script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    //owl carousel script
+//  $('.carousel').owlCarousel({
+//        margin: 20,
+//        loop: true,
+//        autoplayTimeOut: 2000,
+//        autoplayHoverPause: true
+// });
 });
